22a0ddaff72e33826f8c0786afbdbdadf63bc505
sohanmaharjan <81906179+sohanmaharjan@users.noreply.github.com>
2021-05-09 22:29:49 +1000

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -7,12 +7,9 @@ using namespace std;
 
 int main()
 {
-    //display preview screen 
-    //displayPreviewScreen();
-    //Sleep(1000); //sleeps for few seconds befor jumoing into the next code
-    //system("cls"); //clears the screen 
-
-    //display menu
+    
+    displayPreviewScreen();
+   
     displayMenuScreen();
 
     return 0;
